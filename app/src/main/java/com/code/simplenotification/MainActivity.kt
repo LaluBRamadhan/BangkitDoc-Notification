741dd710b41bd4d61bd54b4bf0c0598fb00d0b54
lalubramadhan <lalubagoes2@gmail.com>
2023-09-19 22:29:50 +0700

    implementing pending intent

--- app/src/main/java/com/code/simplenotification/MainActivity.kt
+++ app/src/main/java/com/code/simplenotification/MainActivity.kt
@@ -3,9 +3,14 @@ package com.code.simplenotification
 import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -29,24 +34,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val title = getString(R.string.notification_title)
-        val message = getString(R.string.notification_message)
         binding.btnSendNotification.setOnClickListener{
-            sendNotification(title,message)
+            sendNotification(it)
         }
         if (Build.VERSION.SDK_INT >= 33){
             requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
         }
     }
-
-    private fun sendNotification(title: String, message: String){
+    fun sendNotification(view: View){
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0)
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle(title)
+            .setContentIntent(pendingIntent)
             .setSmallIcon(R.drawable.notification)
-            .setContentText(message)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.notifications_white))
+            .setContentTitle(resources.getString(R.string.notification_title))
+            .setContentText(resources.getString(R.string.notification_message))
             .setSubText(getString(R.string.notification_subtext))
+            .setAutoCancel(true)
 
         if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.O){
             val channel = NotificationChannel(
@@ -61,13 +68,12 @@ class MainActivity : AppCompatActivity() {
         notificationManager.notify(NOTIFICATION_ID, notification)
     }
 
+
     companion object{
         private const val NOTIFICATION_ID = 1
-        private const val CHANNEL_ID = "channel_id"
-        private const val CHANNEL_NAME = "channel_name"
+        private const val CHANNEL_ID = "channel_01"
+        private const val CHANNEL_NAME = "dicoding channel"
     }
-
-
 
 
 }
